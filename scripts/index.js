df5c90c93b19042b689bfbcfa22d259b4864f07b
Bogdan Oshatinskiy <limassola@yandex.ru>
2023-03-04 01:30:58 +0300

    feat: реализовать переключение вкладок

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,11 @@ const popup = document.querySelector('.popup');
 const closeBtn = document.querySelector('.popup__button');
 const popupBtn = document.querySelector('.form__button');
 const openLink = document.querySelector('.nav__item_type_modal');
+const tabLinks = document.querySelectorAll('.details__heading');
+const tabContent = document.querySelectorAll('.details__list');
 
+
+// Popup
 function openPopup() {
     popup.classList.add('popup_opened');
     document.addEventListener('keydown', closeByEscape);
@@ -39,8 +43,28 @@ closeBtn.addEventListener('click', closePopup);
 popupBtn.addEventListener('submit', handleFormSubmit);
 document.addEventListener('mousedown', closeByOverlay);
 
+
+// Slider
 new Swiper('.swiper',{
     pagination: {
         el: '.swiper-pagination',
     }
 });
+
+// Tabs
+tabLinks.forEach((item) => {
+    item.addEventListener('click', (evt) => {
+        evt.preventDefault()
+        const id = evt.target.getAttribute('href').replace('#', '')
+
+        tabLinks.forEach((item) => {
+            item.classList.remove('details__heading_active')
+        });
+        tabContent.forEach((item) => {
+            item.classList.remove('details__list_active')
+        });
+
+        item.classList.add('details__heading_active');
+        document.getElementById(id).classList.add('details__list_active')
+    })
+})
